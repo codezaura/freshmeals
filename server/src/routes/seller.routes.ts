cd35664e07f223a832a617d82e64c38fe9e02510
codezaura <vikashloharjsr@gmail.com>
2026-04-02 00:38:01 +0530

    update: plate adding and plate item adding and removing functionality

--- server/src/routes/seller.routes.ts
+++ server/src/routes/seller.routes.ts
@@ -23,4 +23,15 @@ router.put("/meal/:meal_id", Controller.updateSellerMeal);
 
 router.delete("/meal/:meal_id", Controller.deleteSellerMeal);
 
+router.get("/plate/:plate_id", Controller.getSellerPlate);
+
+router.put("/plate/:plate_id", Controller.updateSellerPlate);
+
+router.patch("/plate/:plate_id/add/:meal_id", Controller.addSellerPlateItem);
+
+router.patch(
+  "/plate/:plate_id/remove/:meal_id",
+  Controller.removeSellerPlateItem,
+);
+
 export default router;
